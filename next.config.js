ca190a9c42efad1830991c32992e811a38ce3a40
Sakib fahmid <hasanfahmidul2002@gmail.com>
2026-01-05 00:39:46 +0600

    Edit

--- next.config.js
+++ next.config.js
@@ -3,6 +3,11 @@ const nextConfig = {
   images: {
     domains: ['lh3.googleusercontent.com'],
   },
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  }
 }
 
 module.exports = nextConfig
